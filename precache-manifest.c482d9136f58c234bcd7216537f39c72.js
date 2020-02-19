e23ae4df03fbfb84759335b0d405acfc3458ddf7
danjosepad <danielpadilhapessoal@outlook.com>
2020-02-19 20:55:33 -0300

    Updates

---
+++ precache-manifest.c482d9136f58c234bcd7216537f39c72.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "808167d2220cda388fbab673ec0a8bc5",
+    "url": "/Portifolio/index.html"
+  },
+  {
+    "revision": "f3905eed32c4cee9cdaa",
+    "url": "/Portifolio/static/css/main.54c11963.chunk.css"
+  },
+  {
+    "revision": "44842c6685d7e5deaca4",
+    "url": "/Portifolio/static/js/2.bd65ef92.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/Portifolio/static/js/2.bd65ef92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3905eed32c4cee9cdaa",
+    "url": "/Portifolio/static/js/main.a28ca640.chunk.js"
+  },
+  {
+    "revision": "c3a6d33a231650d3652b",
+    "url": "/Portifolio/static/js/runtime-main.5ee4935f.js"
+  },
+  {
+    "revision": "290a873467de7ebc51a167e1685f8e7c",
+    "url": "/Portifolio/static/media/anotacoes.290a8734.jpg"
+  },
+  {
+    "revision": "b0ed13da551f9aecb53b7c9900ec8599",
+    "url": "/Portifolio/static/media/coderlogo.b0ed13da.png"
+  },
+  {
+    "revision": "43f9a9fc4e2a40f1ca76e6c13d72e053",
+    "url": "/Portifolio/static/media/fatecOurinhosLogo.43f9a9fc.jpg"
+  },
+  {
+    "revision": "583b91315f23ccfc1445eabb6e6ca641",
+    "url": "/Portifolio/static/media/giphy.583b9131.gif"
+  },
+  {
+    "revision": "f825da680c41d251969d3647b6e5a218",
+    "url": "/Portifolio/static/media/logoGoStack.f825da68.png"
+  },
+  {
+    "revision": "5d5a113044cb4f741bc6f0c10b8c81bf",
+    "url": "/Portifolio/static/media/resizedPhoto.5d5a1130.png"
+  },
+  {
+    "revision": "5da88894158a898210ebc362a7d69420",
+    "url": "/Portifolio/static/media/todolist.5da88894.jpg"
+  },
+  {
+    "revision": "403b9d3d760ed189462c11730bd47dc1",
+    "url": "/Portifolio/static/media/yamanLogo.403b9d3d.png"
+  }
+]);
