d962ef2d8630470e13f4e9c75592246ac171c1a3
daniel.padilha <danielpadilhapessoal@Outlook.com>
2020-10-18 19:58:04 -0300

    chore: added initial state on stackType

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -23,7 +23,7 @@ import styles from './styles.module.css';
 import 'aos/dist/aos.css';
 
 export default function Projetos() {
-  const [stackType, setStackType] = useState('');
+  const [stackType, setStackType] = useState('backend');
 
   useEffect(() => {
     aos.init();
