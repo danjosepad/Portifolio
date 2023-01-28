7912f60a06e813993357b7309ead957b67c8b2ce
danjosepad <danielpadilhapessoal@outlook.com>
2023-01-28 11:03:29 -0300

    feat: add more voices and images with improvements

--- src/pages/funnyTest/index.jsx
+++ src/pages/funnyTest/index.jsx
@@ -4,14 +4,49 @@ import React from 'react';
 
 import { Helmet } from 'react-helmet';
 import useInterval from '../../hooks/useInterval'
+import { Howl } from 'howler'
 
 import Ramon1 from './images/Ramon1.jpg'
 import Ramon2 from './images/Ramon2.jpg'
 import Ramon3 from './images/Ramon3.jpg'
 import Ramon4 from './images/Ramon4.jpg'
-import voice from './sound/voice.mp3'
+import Ramon5 from './images/Ramon5.jpeg'
+import Ramon6 from './images/Ramon6.jpeg'
+import Ramon7 from './images/Ramon7.jpeg'
+import Ramon8 from './images/Ramon8.jpeg'
+import Ramon9 from './images/Ramon9.jpeg'
+import Ramon10 from './images/Ramon10.jpeg'
+import Ramon11 from './images/Ramon11.jpeg'
+import Ramon12 from './images/Ramon12.jpeg'
+import Ramon13 from './images/Ramon13.jpeg'
+import Ramon14 from './images/Ramon14.jpeg'
+import Ramon15 from './images/Ramon15.jpeg'
+import Ramon16 from './images/Ramon16.jpeg'
+import Ramon17 from './images/Ramon17.jpeg'
 
-const images = [Ramon1, Ramon2, Ramon3, Ramon4];
+import voice1 from './sound/voice.mp3'
+import voice2 from './sound/voice2.mp3'
+
+
+const images = [
+  Ramon1, 
+  Ramon2, 
+  Ramon3, 
+  Ramon4, 
+  Ramon5, 
+  Ramon6, 
+  Ramon7, 
+  Ramon8,
+  Ramon9,
+  Ramon10,
+  Ramon11,
+  Ramon12,
+  Ramon13,
+  Ramon14,
+  Ramon15,
+  Ramon16,
+  Ramon17
+];
 
 const getRandomNumber = (min, max) => `${Math.floor(Math.random() * (max - min) + min)}px`
 
@@ -19,15 +54,21 @@ const getRandomImage = () => images[Math.floor(Math.random() * images.length)];
 
 export default function FunnyTest() {
   const [index, setIndex] = React.useState(0);
-  const [delay, setDelay] = React.useState(3000)
+  const [delay, setDelay] = React.useState(3000);
+  const [voiceList, setVoiceList] = React.useState()
+  const [loading, setLoading] = React.useState(true)
 
-  const playSound = (soundfile) => {
-    new Audio(soundfile).play()
-  } 
+  const getRandomVoice = () => voiceList[Math.floor(Math.random() * voiceList.length)];
+
+  const playSound = (soundFile) => {
+    soundFile.play()
+  }
 
   const renderPopupDiv = (index) => {
+    playSound(getRandomVoice())
+
     let div = document.createElement('div');
-    playSound(voice)
+    
     div.setAttribute('id', index)
 
     div.style.position = 'absolute'
@@ -45,10 +86,22 @@ export default function FunnyTest() {
   }
 
   useInterval(() => {
-    renderPopupDiv(index)
-    setIndex(index + 1)
-    if (delay > 1000) setDelay(delay -500)
+    if (!loading) {
+      renderPopupDiv(index)
+      setIndex(index + 1)
+      if (delay > 1000) setDelay(delay -500)
+    }
   }, delay)
+
+  const allowAudio = () => {
+    const howlVoice1 = new Howl({ src: voice1 })
+    const howlVoice2 = new Howl({ src: voice2 })
+
+    setVoiceList([howlVoice1, howlVoice2])
+    setLoading(false)
+  }
+
+  if (loading) return <button onClick={allowAudio}>Permitir áudio</button>
 
   return (
     <div>
@@ -56,6 +109,7 @@ export default function FunnyTest() {
         <title >Ô Brunnão</title>
       </Helmet>
 
+    
     </div>
   );
 }
